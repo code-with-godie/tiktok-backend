3fece846ced754cea14a9d661fa159d82f759d41
godie <ngugimaina2019@gmail.com>
2023-11-15 12:40:06 +0300

    updated server port

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(notFound);
 
 //error handlermindleware
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
+const port = 5000 || process.env.PORT;
 const start = async () => {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
