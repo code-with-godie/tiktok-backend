042bb0d426e05989dac47b507d6e00a7595d11c5
godie <ngugimaina2019@gmail.com>
2023-11-15 12:44:26 +0300

    updated root route

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(helmet());
 app.use(express.json({ limit: 1000000000 }));
 
 //api  routes
-app.get('/api/v1/tiktok-clone', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({ success: true, message: 'tiktok-clone app!!!' });
 });
 app.use('/api/v1/tiktok-clone/users', usersRoutes);
